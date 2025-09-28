306ededd2d477317c67f1c5577626dbe681981e0
Ermakova Natalia <natalysolitonsf@yandex.com>
2025-09-28 14:39:08 +0300

    added test for the task20

---
+++ mantis-tests/appmanager/RegistrationHelper.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+//using Selenium;?
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+            String url = GetConfirmationUrl(account);
+            FillPasswordForm(url, account);
+            SubmitPasswordForm();
+        }
+
+        private void SubmitPasswordForm()
+        {
+            driver.FindElement(By.CssSelector("input.button")).Click();
+        }
+
+        private void FillPasswordForm(string url, AccountData account)
+        {
+            driver.Url = url;
+            driver.FindElement(By.Name("password")).SendKeys(account.Password);
+            driver.FindElement(By.Name("password_confirm")).SendKeys(account.Password);
+        }
+
+        private string GetConfirmationUrl(AccountData account)
+        {
+            String message = manager.Mail.GetLastMail(account);
+            Match match = Regex.Match(message, @"http://\S*");
+            return match.Value;
+        }
+
+        private void OpenRegistrationForm()
+        {
+            driver.FindElements(By.CssSelector("span.bracket-link"))[0].Click();
+        }
+
+        private void SubmitRegistration()
+        {
+            driver.FindElement(By.CssSelector("input.button")).Click();
+        }
+
+        private void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Name("username")).SendKeys(account.Name);
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+        }
+
+        private void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantisbt-1.2.17/login_page.php";
+        }
+    }
+}
